56731e2125f85c2e7ae17b768c0953065ff4ecc6
EC2 Default User <ec2-user@ip-172-31-44-15.ap-northeast-1.compute.internal>
2023-09-06 01:13:16 +0000

    画像投稿機能追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,32 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.end_user_id = current_user.id
+  
+    if @post.save
+      redirect_to root_path
+    else
+      render :new # Re-render the new form if validation fails
+    end
+  end
+
 
   def index
   end
 
   def show
   end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :image, :caption)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
+    resources :posts, only: [:new, :create, :index, :show]
   end
   # 管理者用.
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   ###会員
   devise_for :end_users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   get 'homes/about'
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
